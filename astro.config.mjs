858949f7902379a096d62fc3076dfb9cac927338
adriantaf <atafoya863@gmail.com>
2026-04-12 20:45:45 -0700

    ahora si!

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adriantaf.github.io",
-  base: "/porfolio",
+  base: "/portfolio",
   integrations: [tailwind()],
 });
