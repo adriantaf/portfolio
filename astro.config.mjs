c6b29578b441325ee88e83724319c11282143d8f
adriantaf <atafoya863@gmail.com>
2026-04-12 20:40:42 -0700

    github actions actived

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://adriantaf.github.io',
+  site: "https://adriantaf.github.io",
+  base: "/portfolio",
   integrations: [tailwind()],
 });
