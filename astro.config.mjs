45becf108390f35ac64a4efa8449597fd32d4879
adriantaf <atafoya863@gmail.com>
2026-04-12 20:43:21 -0700

    correccion de nombre del repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://adriantaf.github.io",
-  base: "/portfolio",
+  base: "/porfolio",
   integrations: [tailwind()],
 });
